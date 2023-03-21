cc8007847ab7f769a17df58b48fb31ba667f00db
Johny W. Alves <johnywalves@gmail.com>
2023-03-21 20:33:04 -0300

    :construction:  Clusterizando com cores

--- clustering_images.py
+++ clustering_images.py
@@ -4,12 +4,26 @@ from PIL import Image
 n_columns = 256
 n_rows = 256
 cluster_colors = [
-    (255, 0, 0), (128, 0, 0),
-    (0, 128, 0), (0, 255, 0),
-    (0, 0, 128), (0, 0, 255)
+    (214, 39, 40),
+    (31, 119, 180),
+    (255, 127, 14),
+    (44, 160, 44),
+    (148, 103, 189),
+    (140, 86, 75),
+    (227, 119, 194),
+    (188, 189, 34),
+    (23, 190, 207),
+    (214, 39, 40),
+    (31, 119, 180),
+    (255, 127, 14),
+    (44, 160, 44),
+    (148, 103, 189),
+    (140, 86, 75),
+    (227, 119, 194),
+    (188, 189, 34),
+    (23, 190, 207),
 ]
 
-colors = [200]
 
 def find(labels, x):
     y = x
@@ -29,14 +43,32 @@ def union(labels, x, y):
     labels[find(labels, x)] = find(labels, y)
 
 
-def cluster_image(labels, label, name):
+def cluster_image(name):
     largest_label = 0
-    img = Image.open(name + ".bmp")
+    colors = []
+    colors_label = []
 
-    for c in range(len(colors)):
+    img = Image.open(name + ".bmp")
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            gray = img.getpixel((i, j))
+            if gray not in colors:
+                colors.append(gray)
+
+    for color in colors:
+        # Matrix from image with zeros
+        label = []
+        for x in range(n_columns):
+            label.append([])
+            for y in range(n_rows):
+                label[x].append(0)
+
+        # Array containing integers from 0 to the size of the image
+        labels = list(range(n_columns * n_rows + 1))
+
         for x in range(n_columns):
             for y in range(n_rows):
-                if img.getpixel((x, y)) == colors[c]:
+                if img.getpixel((x, y)) == color:
                     left = 0 if x == 0 else label[x - 1][y]
                     above = 0 if y == 0 else label[x][y - 1]
 
@@ -50,45 +82,35 @@ def cluster_image(labels, label, name):
                     elif (left == 0) and (above != 0):  # One neighbor, above
                         label[x][y] = find(labels, above)
                     else:  # Neighbors BOTH to the left and above
-                        union(labels, left, above)  # Link the left and above clusters
+                        # Link the left and above clusters
+                        union(labels, left, above)
                         label[x][y] = find(labels, left)
+
+        colors_label.append(label)
 
     # Generate clustered
     img_clustered = Image.new('RGB', (256, 256))
     pixels = img_clustered.load()
-    for i in range(img.size[0]):
-        for j in range(img.size[1]):
-            labelred = label[i][j]
-            if (labelred == 0):
-                pixels[i, j] = (200, 200, 200)
-            else:
-                pixels[i, j] = cluster_colors[labelred - 1]
+    for label in colors_label:
+        for i in range(img_clustered.size[0]):
+            for j in range(img_clustered.size[1]):
+                if (label[i][j] != 0):
+                    pixels[i, j] = cluster_colors[label[i][j] - 1]
 
-    img_clustered.save('clustered_' + name + '.bmp')
+    img_clustered.save(name + '_clustered.bmp')
 
-def main(name):
-    label = []
-    for x in range(n_columns):
-        label.append([])
-        for y in range(n_rows):
-            label[x].append(0)
-
-    # Array containing integers from 0 to the size of the image
-    labels = list(range(n_columns * n_rows + 1))
-
-    cluster_image(labels, label, name)
 
 # Clustering image a.bmp
-main('a')
+cluster_image('a')
 
 # Clustering image b.bmp
-main('b')
+cluster_image('b')
 
 # Clustering image c.bmp
-main('c')
+cluster_image('c')
 
 # Clustering image d.bmp
-main('d')
+cluster_image('d')
 
 # Clustering image e.bmp
-main('e')
+cluster_image('e')
